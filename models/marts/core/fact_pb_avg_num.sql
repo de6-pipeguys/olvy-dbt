8f119608bbe1f00658cba2e0ada818f0eec980ee
westg <kwkimsk0630@naver.com>
2025-07-23 14:08:44 +0900

    feat: 카테고리별 최신 데이터만 취급하도록 수정

--- models/marts/core/fact_pb_avg_num.sql
+++ models/marts/core/fact_pb_avg_num.sql
@@ -1,10 +1,28 @@
 {{ config(materialized='table') }}
 
+WITH latest_date_per_category AS (
+    SELECT
+        category,
+        MAX(createdat) AS latest_createdat
+    FROM {{ ref('stg_pb_product_table') }}
+    GROUP BY category
+),
+
+latest_records AS (
+    SELECT s.*
+    FROM {{ ref('stg_pb_product_table') }} s
+    JOIN latest_date_per_category l
+    ON s.category = l.category AND s.createdat = l.latest_createdat
+)
+
 SELECT
     category,
     avgreview,
     numofreviews,
     goodsname,
     createdat
-
-FROM {{ ref('stg_pb_product_table') }}
+FROM latest_records
+WHERE avgreview IS NOT NULL
+AND numofreviews IS NOT NULL
+AND goodsname IS NOT NULL
+AND createdat IS NOT NULL
